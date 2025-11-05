37858c7601139a1366431309d85be553f526d954
Tomas <tomasquiroz1222@gmail.com>
2025-11-05 19:14:02 -0300

    Corrección de errores en slider, diseño responsive mejorado.

--- js/packs-slider.js
+++ js/packs-slider.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dots = document.querySelectorAll('.pack-dot');
+
     const next = document.querySelector('.next-p');
     const prev = document.querySelector('.prev-p');
     const slider = document.querySelector('.packs-slider');
     const slide = document.querySelectorAll('.pack');
-    const eachDot = document.querySelectorAll('.d-1');
+    const packNavDots = document.querySelector('.pack-nav-dots');
 
+    let dots = document.querySelectorAll('.pack-dot');
+    let eachDot = document.querySelectorAll('.d-1');
     let index = 0;
+
+    let extraPackDot = [];
+
+    addExtraPackDot();
 
     console.log(dots.length);
     console.log(slide.length);
@@ -54,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if(Number(dot.dataset.id) === index){
                 dot.classList.add('active');
             }
-        })
+        });
 
         if(index > 0){
             prev.classList.add('active');
@@ -68,4 +74,51 @@ document.addEventListener('DOMContentLoaded', () => {
             next.classList.add('active');
         }
     }
+
+    function addExtraPackDot(){
+
+        extraPackDot.forEach(dot => {
+            dot.remove();
+        })
+        extraPackDot = [];
+        const width = window.innerWidth;
+
+        if(width < 768){
+            createExtraDot();
+            createExtraDot();
+        }else if(width < 1000){
+            createExtraDot();
+        }
+
+        updateDots();
+
+    }
+
+    function updateDots(){
+        dots = document.querySelectorAll('.pack-dot');
+        eachDot = document.querySelectorAll('.d-1');
+    }
+
+    function createExtraDot(){
+
+        const newDot = document.createElement('button');
+        newDot.classList.add('pack-dot');
+        newDot.dataset.dot = document.querySelectorAll('.pack-dot').length + 1;
+        packNavDots.appendChild(newDot);
+
+        const newD1 = document.createElement('span');
+        newD1.classList.add('d-1');
+        newD1.dataset.id = document.querySelectorAll('.d-1').length;
+        newDot.appendChild(newD1);
+
+        extraPackDot.push(newDot);
+
+        updateDots();
+    }
+
+    window.addEventListener('resize', () => {
+        index = 0;
+        updateSlider();
+        addExtraPackDot();
+    });
 })
